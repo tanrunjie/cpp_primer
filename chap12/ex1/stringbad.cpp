565bf812ca70cba581c825e8d55db6481551d242
tanrunjie <309392121@qq.com>
2022-01-07 19:47:21 +0800

    stringbad

--- chap12/ex1/stringbad.cpp
+++ chap12/ex1/stringbad.cpp
@@ -5,29 +5,34 @@ using namespace std;
 
 int StringBad::num_strings = 0;
 
+int StringBad::HowMany()
+{
+    return num_strings;
+}
+
 StringBad::StringBad(const char * s)
 {
     len = strlen(s);
     str = new char[len+1];
     strcpy(str, s);
     num_strings++;
-    cout <<num_strings<<": \"" << str <<"\" object created\n";
+    // cout <<num_strings<<": \"" << str <<"\" object created\n";
 }
+
 
 StringBad::StringBad()
 {
-    len = 4;
-    str = new char[len];
-    strcpy(str,"c++");
+    str = new char[1];  //兼容 delete [] str
+    str[0] = '\0';
     num_strings++;
-    cout <<num_strings<<": \"" << str <<"\" object created\n";
+    // cout <<num_strings<<": \"" << str <<"\" object created\n";
 }
 
 StringBad::~StringBad()
 {
-    cout << "\""<< str <<"\" object deleted, ";
+    // cout << "\""<< str <<"\" object deleted, ";
     --num_strings;
-    cout <<num_strings << " left\n";
+    // cout <<num_strings << " left\n";
     delete [] str;
 }
 
@@ -35,4 +40,67 @@ ostream & operator<<(ostream & os, const StringBad & st)
 {
     os << st.str;
     return os;
+}
+
+istream & operator>>(istream & is, StringBad & st)
+{
+    char temp[StringBad::CINLIM];
+    is.get(temp, StringBad::CINLIM);
+    if(is)
+        st = temp;
+    while(is && is.get()!='\n')
+        continue;
+    return is;
+}
+
+
+// 拷贝函数深复制
+StringBad::StringBad(const StringBad & st)
+{
+  num_strings++;
+  len = st.len;
+  str = new char[len+1];
+  std::strcpy(str, st.str);  // 复制构造，深复制而非隐式浅复制
+}
+
+
+StringBad & StringBad::operator=(const char * s)
+{
+    delete []str;
+    len = strlen(s);
+    str = new char[len+1];
+    strcpy(str,s);
+    return *this;
+}
+
+// 赋值深复制:并非创建对象，而是对已有对象操作
+StringBad & StringBad::operator=(const StringBad & st)
+{
+  if(this == &st)  // assigned to itself
+    return *this;
+  delete [] str; // free old
+  len = st.len;
+  str = new char [len+1];
+  strcpy(str, st.str);
+  return *this;
+}
+
+
+bool operator<(const StringBad & st1, const StringBad & st2)
+{
+    return (strcmp(st1.str, st2.str) < 0);
+}
+
+bool operator>(const StringBad & st1, const StringBad & st2)
+{
+    return (strcmp(st1.str, st2.str) > 0);
+}
+bool operator==(const StringBad & st1, const StringBad & st2)
+{
+    return (strcmp(st1.str, st2.str) == 0);
+}
+
+char & StringBad::operator[](int i)
+{
+    return str[i];
 }
--- chap12/ex1/stringbad.h
+++ chap12/ex1/stringbad.h
@@ -9,13 +9,26 @@ class StringBad
         char * str;
         int len;
         static int num_strings;
+        static const int CINLIM = 80;
     public:
         StringBad(const char * s);
+        StringBad(const StringBad & s);
+
         StringBad();
         ~StringBad();
         friend std::ostream & operator<<(std::ostream & os, const StringBad & st);
-    
+        friend std::istream & operator>>(std::istream & is, StringBad & st);
+        // important: deep copy
+        StringBad & operator=(const StringBad & st);
+        StringBad & operator=(const char * s);
+        //extent
+        int length() const { return len;}
+        friend bool operator<(const StringBad & st1, const StringBad & st2);
+        friend bool operator>(const StringBad & st1, const StringBad & st2);
+        friend bool operator==(const StringBad & st1, const StringBad & st2);
+        // friend istream & operator>>(istream & is, StringBad & st);
+        char & operator[] (int i);
+        const char & operator[] (int i) const;
+        static int HowMany();
 };
-
-
 #endif
